15611d101a5b0f2721e4c311801db0f545e9c078
Bùi Xuân Hoàng Oanh <62228385+HoangOanh0711@users.noreply.github.com>
2023-06-23 00:20:27 +0700

    sua UI tin tuc

---
+++ ts910/News/Card_New.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace ts910.News
+{
+    partial class Card_New
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.img_news = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.guna2Panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.img_news)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Panel1.BorderRadius = 10;
+            this.guna2Panel1.Controls.Add(this.label4);
+            this.guna2Panel1.Controls.Add(this.label3);
+            this.guna2Panel1.Controls.Add(this.img_news);
+            this.guna2Panel1.FillColor = System.Drawing.Color.White;
+            this.guna2Panel1.Location = new System.Drawing.Point(2, 2);
+            this.guna2Panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(407, 418);
+            this.guna2Panel1.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(5, 288);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(395, 133);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Thầy Võ Thiện Cang - Trưởng phòng Phòng Khảo thí và Kiểm định Chất lượng Giáo dục" +
+    " Sở Giáo dục và Đào tạo TP. HCM chia sẻ các kênh thông tin hỗ trợ phụ huynh và h" +
+    "ọc sinh suốt kỳ thi tuyển sinh 10";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(9, 238);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(391, 60);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "CÁC KÊNH THÔNG TIN HỖ TRỢ PHỤ HUYNH VÀ HỌC SINH SUỐT KỲ THI TUYỂN SINH 10";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // img_news
+            // 
+            this.img_news.BorderRadius = 10;
+            this.img_news.ErrorImage = global::ts910.Properties.Resources.new1;
+            this.img_news.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.img_news.Image = global::ts910.Properties.Resources.new1;
+            this.img_news.ImageRotate = 0F;
+            this.img_news.Location = new System.Drawing.Point(9, 8);
+            this.img_news.Margin = new System.Windows.Forms.Padding(2);
+            this.img_news.Name = "img_news";
+            this.img_news.Size = new System.Drawing.Size(391, 216);
+            this.img_news.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img_news.TabIndex = 0;
+            this.img_news.TabStop = false;
+            // 
+            // Card_New
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.guna2Panel1);
+            this.Name = "Card_New";
+            this.Size = new System.Drawing.Size(416, 429);
+            this.guna2Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.img_news)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private Guna.UI2.WinForms.Guna2PictureBox img_news;
+    }
+}
